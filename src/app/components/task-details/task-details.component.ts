24b78e0747b482d79d5ef12331d468b71828c299
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-11-25 18:21:13 +0530

    task-details

--- src/app/components/task-details/task-details.component.ts
+++ src/app/components/task-details/task-details.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Task } from '../../model/task.model';
+import { TaskService } from '../../service/task.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
 export class TaskDetailsComponent {
+    task? : Task;
 
+    constructor(
+      private _taskService : TaskService,
+      private _route : ActivatedRoute,
+      private _router : Router
+    ){}
+
+    ngOnInit() : void{
+      const id = this._route.snapshot.paramMap.get('id')
+      if(id){
+        const taskId =+id;
+        this.task = this._taskService.getTask(taskId)
+      }
+    }
 }
