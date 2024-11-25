eca6319f2b3b60f62cb483c0c2fdba5167845c24
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-11-25 19:32:47 +0530

    edit done

--- src/app/components/task-details/task-details.component.ts
+++ src/app/components/task-details/task-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../../model/task.model';
 import { TaskService } from '../../service/task.service';
 import { ActivatedRoute } from '@angular/router';
@@ -9,22 +9,22 @@ import { CommonModule } from '@angular/common';
   selector: 'app-task-details',
   imports: [CommonModule],
   templateUrl: './task-details.component.html',
-  styleUrl: './task-details.component.css'
+  styleUrl: './task-details.component.css',
 })
-export class TaskDetailsComponent {
-    task? : Task;
+export class TaskDetailsComponent implements OnInit {
+  task?: Task;
 
-    constructor(
-      private _taskService : TaskService,
-      private _route : ActivatedRoute,
-      private _router : Router
-    ){}
+  constructor(
+    private _taskService: TaskService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {}
 
-    ngOnInit() : void{
-      const id = this._route.snapshot.paramMap.get('id')
-      if(id){
-        const taskId =+id;
-        this.task = this._taskService.getTask(taskId)
-      }
+  ngOnInit(): void {
+    const id = this._route.snapshot.paramMap.get('id');
+    if (id) {
+      const taskId = +id;
+      this.task = this._taskService.getTask(taskId);
     }
+  }
 }
